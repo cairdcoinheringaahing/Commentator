d86a1986f6a4ca35362ad1d8bb241fc6e103f163
Satan's Son <valyrio.email@gmail.com>
2017-07-17 04:45:31 +0100

    Added REPL environment

--- interpreter.py
+++ interpreter.py
@@ -28,28 +28,32 @@ def run(code, acc, acc_active, a, printed, *args):
             try:
                 acc[acc_active] += args[a]
             except:
-                acc[acc_active] += acc[1^acc_active]
+                acc[acc_active] += acc[(acc_active + 1) % 20]
             a += 1
         if char == "--":
             acc[acc_active] = is_prime(acc[acc_active])
         if char == "/*":
             acc[acc_active] = 0
         if char == "*/":
-            print(end=acc[acc_active])
+            print(end=str(acc[acc_active]))
             printed = True
         if char == '<#':
-            acc_active = 1 ^ acc_active
+            acc_active = (acc_active + 1) % 20
         if char == '<!':
-            acc[acc_active] = acc[acc_active] // acc[acc_active]
+            acc[acc_active] = acc[acc_active] // acc[(acc_active + 1) % 20]
         if char == 'e#':
             acc[acc_active] = 0 - acc[acc_active]
         if char == '{-':
             acc[acc_active] = acc[acc_active] ^ 1
+        if char == '-}':
+            acc_active = (acc_active - 1) % 20
+            if acc_active < 0:
+                acc_active = 21 + acc_active
             
         if char == '!':
-            acc[acc_active] = acc[acc_active] * acc[1^acc_active]
+            acc[acc_active] = acc[acc_active] * acc[(acc_active + 1) % 20]
         if char == '%':
-            acc[acc_active] = acc[acc_active] % acc[1^acc_active]
+            acc[acc_active] = acc[acc_active] % acc[(acc_active + 1) % 20]
         if char == " ":
             acc[acc_active] += 1
         if char == "#":
@@ -59,16 +63,16 @@ def run(code, acc, acc_active, a, printed, *args):
     return acc, acc_active, a, printed
 
 def interpreter(code, *args):
-    acc = [0,0]
+    acc = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
     acc_active = 0
     a = 0
     printed = False
     for line in parser(code):
         if line[0] == ';':
-            for i in range(acc[active]):
+            for i in range(acc[acc_active]):
                 acc, acc_active, a, printed = run(line, acc, acc_active, a, printed, *args)
         elif line[0] == ':':
-            if acc[active]:
+            if acc[acc_active]:
                 acc, acc_active, a, printed = run(line, acc, acc_active, a, printed, *args)
         elif line[0] == '?':
             while acc[acc_active]:
@@ -76,8 +80,9 @@ def interpreter(code, *args):
         else:
             acc, acc_active, a, printed = run(line, acc, acc_active, a, printed, *args)
     if not printed:
-        print(chr(acc[acc_active]))
-        
+        print(acc[acc_active])
+    return printed
+
 def eval_input(args):
     final = []
     for arg in args:
@@ -87,10 +92,66 @@ def eval_input(args):
             continue
     return final
 
+def REPL(helpmessage,lastcode=''):
+
+    if helpmessage:
+        print('''Welcome to Commentator REPL environment!
+This allows you to enter and run Commentator code,
+without having to run from the command line!
+
+When >>> appears on the screen, this is asking you for a line of code.
+This will continue to ask for code, until you enter a blank line
+
+You can also begin a line of code with a § symbol
+This will create a single line of code, and goes straight the input
+
+Then it will ask you for inputs (a > will appear)
+Please enter each input on a separate prompt
+
+The output for the program will then be displayed below.
+
+Enter 'exit' to quit the REPL.
+
+''')
+
+    code = []
+    line = input('>>> ')
+    if not line:
+        print(line)
+        REPL(False)
+    if line == 'exit':
+        return
+    if line[0] == '§':
+        code = line[1:]
+        line = False
+    if line == '^':
+        code = last_code
+    
+    while line:
+        code.append(line)
+        line = input('>>> ')
+
+    if line == '':
+        code = '\n'.join(code)
+
+    inputs = []
+    for i in range(code.count('//')):
+        last_in = input('> ')
+        inputs.append(last_in)
+    inputs = eval_input(inputs)
+
+    if interpreter(code, *inputs):
+        print()
+
+    REPL(False,code)
+
 if __name__ == "__main__":
     import sys
-    program = sys.argv[1]
-    args = eval_input(sys.argv[2:])
-    if program.endswith(".txt"):
-        program = open(program).read()
-    interpreter(program, *args)
+    try:
+        program = sys.argv[1]
+        args = eval_input(sys.argv[2:])
+        if program.endswith(".txt"):
+            program = open(program).read()
+        interpreter(program, *args)
+    except:
+        REPL(True)
